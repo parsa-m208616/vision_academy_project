19675935f3f5626f6155c87ed6ab602f92af3821
parsa <parsa.m208616@gmail.com>
2024-07-10 12:04:55 +0330

    models added

--- product/models.py
+++ product/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    parent = models.ForeignKey('self', on_delete=models.CASCADE, blank=True, null=True)
+    title = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    avatar = models.ImageField(blank=True, upload_to='categories/')
+    is_enable = models.BooleanField(default=True)
+    pub_date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.title
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    avatar = models.ImageField(blank=True, upload_to='products/')
+    is_enable = models.BooleanField(default=True)
+    categories = models.ManyToManyField(Category, blank=True)
+    pub_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+
+class File(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    file = models.FileField(upload_to='files/%Y/%m/')
+    is_enable = models.BooleanField(default=True)
+    pub_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+
